709a46586de2972a44d80256447fedabe3f4d6ea
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-10-30 21:10:31 +0500

    working on chat section

--- src/components/Chats/Chat/Chat.jsx
+++ src/components/Chats/Chat/Chat.jsx
@@ -1,8 +1,21 @@
+import { Avatar } from '@material-ui/core'
+import { StopRounded } from '@material-ui/icons'
+import ReactTimeago from 'react-timeago'
 import './Chat.css'
-function Chat() {
+function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
+    
+    const open = () => {
+        
+    }
+
     return (
-        <div className="chat">
-            
+        <div onClick={open} className="chat">
+            <Avatar className="chat__avatar" src={profilePic}/>
+            <div className="chat__info">
+                <h4>{username}</h4>
+                <p>Tap to view - <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()}/></p>
+            </div>
+            {!read && <StopRounded className="chat__readIcon"/>}
         </div>
     )
 }
